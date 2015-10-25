49594a0b596a443406e6e5434f107d348257f344
Ilya <ilyadorosh@gmail.com>
2015-10-26 01:19:50 +0300

    Create scrap.js

---
+++ scrap.js
@@ -0,0 +1,11 @@
+<img class="invrt_send" src="http://nevesti.com.ua/wp-content/uploads/2015/10/mes2.jpg" alt="" />
+<script type="text/javascript">// <![CDATA[
+/* Init chat */ jQuery(document).on('click', '.invrt_send', function(){ chat_from = jQuery('.userpro-init-chat').data('chat_from'); chat_with = jQuery('.userpro-init-chat').data('chat_with');; up_msg_overlay( chat_from, chat_with ); });
+
+jQuery(".invrt_send").after( jQuery('#rate-10')[0] );
+jQuery(".userpro-nostyle").hide();
+jQuery( document ).ready(function() {
+    jQuery("#rate-10").children().width(65);
+  });
+// ]]>
+</script>
