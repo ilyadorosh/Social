09aeb1ac03b5bd0697ce6b2211a17e091d9f5250
Ilya <ilyadorosh@gmail.com>
2015-10-26 01:55:08 +0300

    Update scrap.js

--- scrap.js
+++ scrap.js
@@ -1,11 +1,22 @@
-<img class="invrt_send" src="http://nevesti.com.ua/wp-content/uploads/2015/10/mes2.jpg" alt="" />
+<img class="invrt_send" src="http://nevesti.com.ua/wp-content/uploads/2015/10/3.jpg" alt="" />
+<img class="invrt_money" src="http://nevesti.com.ua/wp-content/uploads/2015/10/2.jpg" alt="" />
+<img class="invrt_gift" src="http://nevesti.com.ua/wp-content/uploads/2015/10/1.jpg" alt="" />
+<img class="invrt_top" src="http://nevesti.com.ua/wp-content/uploads/2015/10/00.jpg" alt="" />
 <script type="text/javascript">// <![CDATA[
 /* Init chat */ jQuery(document).on('click', '.invrt_send', function(){ chat_from = jQuery('.userpro-init-chat').data('chat_from'); chat_with = jQuery('.userpro-init-chat').data('chat_with');; up_msg_overlay( chat_from, chat_with ); });
 
-jQuery(".invrt_send").after( jQuery('#rate-10')[0] );
-jQuery(".userpro-nostyle").hide();
-jQuery( document ).ready(function() {
-    jQuery("#rate-10").children().width(65);
+jQuery(".invrt_top").mouseover(function() {
+    $( this ).attr( "src" ,"http://nevesti.com.ua/wp-content/uploads/2015/10/Love.png");
   });
+
+jQuery(".invrt_send").before( jQuery('#rate-10')[0] );
+jQuery(".userpro-nostyle").hide();
+
+jQuery( document ).ready(function() {
+jQuery("#rate-10").children().width(65);
+jQuery("#rate-10").children().attr( "src" ,"http://nevesti.com.ua/wp-content/uploads/2015/10/001.jpg");
+});
+
+
 // ]]>
 </script>
